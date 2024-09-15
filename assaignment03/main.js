745cdee8affd86e016c5022a82fe3342ea47a0b5
SadafMustafa981 <sadafmustafa39@gmail.com>
2024-09-15 11:23:54 -0700

    Add files via upload

---
+++ assaignment03/main.js
@@ -0,0 +1,37 @@
+// Assaignment of Array
+// QUIESTION NO 01
+var fruits = ["apple", "banana", "mango", "orange"];
+console.log(fruits);
+//QUESTION NO 02
+var numbers = [10, 20, 30, 40];
+console.log(numbers);
+// QUESTION NO 03
+var thirdFruit = fruits[2];
+console.log(thirdFruit);
+// QUESTION NO 04
+numbers[1] = 25;
+console.log(numbers);
+// QUESTION NO 05
+fruits.push("Grape");
+console.log(fruits);
+// QUESTION NO 06
+var lastFruit = fruits.pop();
+console.log(fruits);
+// QUESTION NO 07
+var firstFruit = fruits.shift();
+console.log(fruits);
+// QUESTION NO 08
+fruits.unshift("kiwi");
+console.log(fruits);
+// QUESTION NO 09
+fruits.splice(1, 2);
+console.log(fruits);
+// QUESTION NO 10
+fruits.splice(2, 0, "pineapple", "pear");
+console.log(fruits);
+// QUESTION NO 11
+var citrusFruit = fruits.slice(0, 2);
+console.log(citrusFruit);
+// question no 12
+var lastTwoFruits = fruits.slice(-2);
+console.log(lastTwoFruits);
---
+++ assaignment03/main.ts
@@ -0,0 +1,72 @@
+// Assaignment of Array
+// QUIESTION NO 01
+
+const fruits : string [] = ["apple", "banana", "mango", "orange"];
+console.log(fruits);
+
+
+//QUESTION NO 02
+
+const numbers: number[] = [10, 20, 30, 40];
+console.log(numbers);
+
+
+// QUESTION NO 03
+
+const thirdFruit = fruits[2];
+console.log(thirdFruit);
+
+
+// QUESTION NO 04
+
+numbers[1] = 25;
+console.log(numbers);
+
+
+// QUESTION NO 05
+
+fruits.push("Grape");
+console.log(fruits);
+
+
+// QUESTION NO 06
+
+const lastFruit = fruits.pop();
+console.log(fruits);
+
+
+// QUESTION NO 07
+
+const firstFruit = fruits.shift();
+console.log(fruits);
+
+
+// QUESTION NO 08
+
+fruits.unshift("kiwi");
+console.log(fruits);
+
+
+// QUESTION NO 09
+
+fruits.splice(1,2);
+console.log(fruits);
+
+
+// QUESTION NO 10
+
+fruits.splice(2,0 , "pineapple", "pear");
+console.log(fruits);
+
+
+// QUESTION NO 11
+
+const citrusFruit = fruits.slice(0,2);
+console.log(citrusFruit);
+
+
+// question no 12
+
+const lastTwoFruits = fruits.slice(-2);
+console.log(lastTwoFruits);
+
